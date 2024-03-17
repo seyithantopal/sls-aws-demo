5a9a66efcde044dfe6a83a5a528450e90c7151dd
seyithan.topal <18354002+seyithantopal@users.noreply.github.com>
2024-03-17 12:47:37 +0100

    chore: another test

--- src/users/lambdas/createUser.ts
+++ src/users/lambdas/createUser.ts
@@ -13,6 +13,21 @@ export const handler = async (
   console.log('createUser lambda');
   // const payload: User = JSON.parse(event.body as string);
 
+  const stateMachineArn =
+    'arn:aws:states:eu-central-1:851725547947:stateMachine:createUserStepFunction';
+
+  const params: StartExecutionCommandInput = {
+    stateMachineArn,
+    input: JSON.stringify({
+      id: 1,
+    }),
+  };
+
+  const response = await new SFNClient({}).send(
+    new StartExecutionCommand(params),
+  );
+  console.log(response);
+
   return {
     statusCode: 200,
     body: JSON.stringify({
