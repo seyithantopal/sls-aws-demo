1a4d51f51b550af88b3397cb7bcff94002255d33
seyithan.topal <18354002+seyithantopal@users.noreply.github.com>
2024-03-17 11:07:34 +0100

    feat: implement the step functions in createUser

--- src/users/lambdas/createUser.ts
+++ src/users/lambdas/createUser.ts
@@ -1,30 +1,36 @@
-import { createUser } from '@users/controllers/createUser.controller';
 import { User } from '@users/types/User';
 import { APIGatewayEvent, APIGatewayProxyResult, Context } from 'aws-lambda';
+import {
+  SFNClient,
+  StartExecutionCommand,
+  StartExecutionCommandInput,
+} from '@aws-sdk/client-sfn';
 
 export const handler = async (
   event: APIGatewayEvent,
   { invokedFunctionArn }: Context,
 ): Promise<APIGatewayProxyResult> => {
-  try {
-    const payload: User = JSON.parse(event.body as string);
+  console.log('createUser lambda');
+  const payload: User = JSON.parse(event.body as string);
 
-    return new Promise((resolve) => {
-      resolve({
-        statusCode: 200,
-        body: JSON.stringify({
-          message: 'User created successfully',
-        }),
-      });
-    });
-  } catch (error) {
-    return new Promise((resolve) => {
-      resolve({
-        statusCode: 500,
-        body: JSON.stringify({
-          message: 'Something went wrong during creating user',
-        }),
-      });
-    });
-  }
+  const stateMachineArn =
+    'arn:aws:states:eu-central-1:851725547947:stateMachine:createUserStepFunction';
+
+  const params: StartExecutionCommandInput = {
+    stateMachineArn,
+    input: JSON.stringify(payload),
+  };
+
+  const response = await new SFNClient({}).send(
+    new StartExecutionCommand(params),
+  );
+  console.log(response);
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'User created successfully',
+      payload: response,
+    }),
+  };
 };
