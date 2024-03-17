e5f6fa238165b5bfb8dccd91ec86981932c0d001
seyithan.topal <18354002+seyithantopal@users.noreply.github.com>
2024-03-17 13:12:32 +0100

    chore test

--- src/users/lambdas/getUserData.ts
+++ src/users/lambdas/getUserData.ts
@@ -9,36 +9,26 @@ import {
 export const handler = async (
   event: APIGatewayEvent,
 ): Promise<APIGatewayProxyResult> => {
-  try {
-    const payload: User = JSON.parse(event.body as string);
+  const payload: User = JSON.parse(event.body as string);
 
-    const stateMachineArn =
-      'arn:aws:states:eu-central-1:851725547947:stateMachine:createUserStepFunction';
+  const stateMachineArn =
+    'arn:aws:states:eu-central-1:851725547947:stateMachine:createUserStepFunction';
 
-    const params: StartExecutionCommandInput = {
-      stateMachineArn,
-      input: JSON.stringify(payload),
-    };
+  const params: StartExecutionCommandInput = {
+    stateMachineArn,
+    input: JSON.stringify(payload),
+  };
 
-    const response = await new SFNClient({}).send(
-      new StartExecutionCommand(params),
-    );
-    console.log(response);
+  const response = await new SFNClient({})
+    .send(new StartExecutionCommand(params))
+    .catch((res) => console.log('error is occured'));
+  console.log(response);
 
-    return {
-      statusCode: 200,
-      body: JSON.stringify({
-        message: 'User data fetched successfully',
-        payload: response,
-      }),
-    };
-  } catch (error) {
-    console.error('error: ', error);
-    return {
-      statusCode: 500,
-      body: JSON.stringify({
-        message: 'Something went wrong during getting user data',
-      }),
-    };
-  }
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'User data fetched successfully',
+      payload: response,
+    }),
+  };
 };
