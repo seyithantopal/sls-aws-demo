5708fcf81482b753b81a4a980785469cb447bd04
seyithan.topal <18354002+seyithantopal@users.noreply.github.com>
2024-03-17 21:25:44 +0100

    chore: change the boolean value and add isExisted

--- src/users/lambdas/get-user-data.ts
+++ src/users/lambdas/get-user-data.ts
@@ -2,12 +2,23 @@ import { saveUser } from '@users/controllers/saveUser.controller';
 import { User } from '@users/types/User';
 import { APIGatewayEvent, APIGatewayProxyResult, Context } from 'aws-lambda';
 
+type CreateUserStepFunctionInput = {
+  id: string;
+  username: string;
+  email: string;
+  age: number;
+  isExisted: boolean;
+};
+
 export const handler = async (
-  event: APIGatewayEvent,
-): Promise<APIGatewayEvent> => {
+  event: CreateUserStepFunctionInput,
+): Promise<CreateUserStepFunctionInput> => {
   console.log('get-user-data handler event: ', {
     event,
   });
 
-  return event;
+  return {
+    ...event,
+    isExisted: true,
+  };
 };
